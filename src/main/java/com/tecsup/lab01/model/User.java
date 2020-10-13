c7a9deedef55fdc0e0f52f1373e68445848719e9
adrian.lujan <adrian.lujan@tecsup.edu.pe>
2020-10-13 16:50:04 -0500

    Parte Controladora

--- src/main/java/com/tecsup/lab01/model/User.java
+++ src/main/java/com/tecsup/lab01/model/User.java
@@ -1,5 +1,13 @@
 package com.tecsup.lab01.model;
 
-public class User {
+import com.tecsup.lab01.model.*;
 
+public class User {
+	
+	public static void main( String[] args) {
+		User u = new User();
+		System.out.println("Esteban");
+		System.out.println("Quito");
+		System.out.println("18");
+	}
 }
