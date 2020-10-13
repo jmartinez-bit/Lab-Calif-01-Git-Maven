f865f8e799fbde52fb23409d35da5cefd3ff7411
Lincoln Martinez <lincoln.martinez@tecsup.edu.pe>
2020-10-13 16:00:36 -0500

    First commit

---
+++ src/main/java/com/tecsup/lab01/util/MyString.java
@@ -0,0 +1,5 @@
+package com.tecsup.lab01.util;
+
+public class MyString {
+
+}
---
+++ src/test/java/com/tecsup/lab01/Lab_Calif_01_Git_Maven/AppTest.java
@@ -0,0 +1,20 @@
+package com.tecsup.lab01.Lab_Calif_01_Git_Maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
